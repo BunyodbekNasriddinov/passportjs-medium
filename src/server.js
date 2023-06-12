75741587a574ed44f8038ce067af3f590f2a4ddc
BunyodbekNasriddinov <nasriddinoffbunydobek@gmail.com>
2023-06-12 16:05:36 +0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+
+import { resolve } from "path"
+import allRotes from "./routes/all.routes.js"
+
+const app = express()
+
+const PORT = process.env.PORT || 5001
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(resolve("/uploads")))
+
+app.use(allRotes)
+
+app.listen(PORT, () => console.log(`Server running ${PORT}`))
